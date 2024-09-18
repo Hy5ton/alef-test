d76f91b8ca3da63954e60a16f032732b71d8b0b3
Huston <OnlyHuston@gmail.com>
2024-09-18 19:25:19 +0400

    [App] wip: Создать заготовку

--- src/components/PersonalData.vue
+++ src/components/PersonalData.vue
@@ -4,7 +4,20 @@
       <div class="label">Персональные данные</div>
       <text-field label='Имя' class="user" style="margin-top: 3vh;" />
       <text-field label='Возраст' class="user" />
-      <v-btn></v-btn>
+      <div style="display: flex;">
+        <div class="label">Дети (макс. 5)</div>
+        <v-btn v-if="childrenList.length !== 5" @click="addChildren" rounded size="large" outlined style="color: rgba(1, 167, 253, 1);" class="add-children">
+          <v-icon dark left> mdi-plus </v-icon>
+          Добавить ребенка
+        </v-btn>
+      </div>
+      <div v-for="(children, index) in childrenList" :key="children.id" style="margin-top: 3vh;" class="child-list">
+        <text-field v-model="childrenList[index].name" label='Имя' />
+        <text-field v-model="childrenList[index].age" label='Возраст' />
+        <v-btn @click="deleteChildren(index)" rounded size="large" text style="margin-top: 1vh; color: rgba(1, 167, 253, 1);" class="add-children">
+          Удалить
+        </v-btn>
+      </div>
     </div>
   </v-container>
 </template>
@@ -17,6 +30,28 @@ export default {
 
   components: { 
     TextField 
+  },
+
+  data() {
+    return {
+      childrenList: [],
+    }
+  },
+
+  methods: {
+    addChildren() {
+      this.childrenList.push({
+        name: null,
+        age: null,
+      })
+    },
+
+    deleteChildren(index) {
+      console.log(this.childrenList)
+      console.log(index)
+      this.childrenList.splice(index + 1, 1)
+      console.log(this.childrenList)
+    }
   }
 }
 </script>
@@ -28,11 +63,20 @@ export default {
 }
 
 .user {
-  width: 50%;
   height: 13%;
 }
 
 .personal-data {
   margin-top: 4vh;
+  width: 50vw;
+}
+
+.add-children {
+  margin-left: auto;
+}
+
+.child-list {
+  display: flex;
+  gap: 5%;
 }
 </style>
--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,13 +1,30 @@
 <template>
-  <v-text-field :label='label' outlined />
+  <v-text-field :label='label' @input="handleInput" outlined />
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld', 
+  name: 'TextField', 
 
   props: {
     label: { type: String, default: '' },
+  },
+
+  data () {
+    return {
+      timeout: null,
+    }
+  },
+
+  methods: {
+    handleInput (event) {
+      if (this.timeout) {
+        clearTimeout(this.timeout)
+      }
+      this.timeout = setTimeout(() => {
+        this.$emit('input', event)
+      }, 100)
+    },
   }
 }
 </script>
